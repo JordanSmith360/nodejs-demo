537e1e2caf23733ec9ece597971e5edf0e9420f3
Jordan Smith <Jordan@synthesis.co.za>
2022-12-13 17:31:15 +0200

    Updated use use src folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import users from "./api/users";
+import users from "./apis/users.js";
 
 dotenv.config();
 
@@ -9,6 +9,10 @@ const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at https://localhost:${port}`);
+});
+
+app.get("/", async (_, res) => {
+  res.send("Hello World");
 });
 
 app.use("/user", users);
